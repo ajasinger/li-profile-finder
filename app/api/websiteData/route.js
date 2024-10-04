9d9ef7adabd6052b5e02c096433ba44a9f81b476
Aja Singer <ajasinger@gmail.com>
2024-10-04 17:39:59 -0400

    code pupeteer fetch

--- app/api/websiteData/route.js
+++ app/api/websiteData/route.js
@@ -1,11 +1,38 @@
 import { NextResponse } from 'next/server';
+import puppeteer from 'puppeteer';
 
 export async function POST(request) {
     const {name} = await request.json();
 
-    const res = await fetch('https://www.linkedin.com/in/ajasinger');
-    const html = res.text();
-    console.log('html', html);
+    try{
+        // Launch the browser, open a new blank page, navigate the page to a URL & wait for page to load
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+        await page.goto('https://www.linkedin.com/in/ajasinger', {
+            waitUntil: 'networkidle2',
+          });
+
+        // Extract data from the page using evaluate
+        const profileData = await page.evaluate(() => {
+            // You can customize the selectors based on what you need to scrape
+            const name = document.querySelector('.pv-text-details__left-panel h1')?.innerText;
+            const headline = document.querySelector('.text-body-medium')?.innerText;
+            const location = document.querySelector('.pv-top-card--list-bullet li')?.innerText;
+            return {
+            name,
+            headline,
+            location,
+            };
+        });
+
+        // Log the scraped profile data
+        console.log('Profile Data:', profileData);
+        
+        await browser.close();
+
+    }catch(error) {
+        console.log(error)
+    }
 
     return NextResponse.json("");
 }
