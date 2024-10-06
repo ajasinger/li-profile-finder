abfeefa31709d3d7ca9e084eb0ca0132f910fb5d
Aja Singer <ajasinger@gmail.com>
2024-10-06 15:38:29 -0400

    testing code

--- app/api/profileData/route.js
+++ app/api/profileData/route.js
@@ -1,34 +1,45 @@
 import { NextResponse } from 'next/server';
 import puppeteer from 'puppeteer';
+import { cookies } from 'next/headers';
 
 export async function POST(request) {
     const {name} = await request.json();
 
     try{
+
+        // const cookie = cookies()
+        // const accessToken = cookie.get('access_token')
+
+        const res = await fetch('https://www.linkedin.com/in/ceonyc');
+        const html = await res.text();
+
+        console.log(html);
+
+
         // Launch the browser, open a new blank page, navigate the page to a URL & wait for page to load
-        const browser = await puppeteer.launch({ headless: false });
-        const page = await browser.newPage();
-        await page.goto('https://www.linkedin.com/in/ajasinger', {
-            waitUntil: 'networkidle2',
-          });
+        // const browser = await puppeteer.launch({ headless: false });
+        // const page = await browser.newPage();
+        // await page.goto('https://www.linkedin.com/in/ajasinger', {
+        //     waitUntil: 'networkidle2',
+        //   });
 
-        // Extract data from the page using evaluate
-        const profileData = await page.evaluate(() => {
-            // You can customize the selectors based on what you need to scrape
-            const name = document.querySelector('.pv-text-details__left-panel h1')?.innerText;
-            const headline = document.querySelector('.text-body-medium')?.innerText;
-            const location = document.querySelector('.pv-top-card--list-bullet li')?.innerText;
-            return {
-            name,
-            headline,
-            location,
-            };
-        });
+        // // Extract data from the page using evaluate
+        // const profileData = await page.evaluate(() => {
+        //     // You can customize the selectors based on what you need to scrape
+        //     const name = document.querySelector('.pv-text-details__left-panel h1')?.innerText;
+        //     const headline = document.querySelector('.text-body-medium')?.innerText;
+        //     const location = document.querySelector('.pv-top-card--list-bullet li')?.innerText;
+        //     return {
+        //     name,
+        //     headline,
+        //     location,
+        //     };
+        // });
 
-        // Log the scraped profile data
-        console.log('Profile Data:', profileData);
+        // // Log the scraped profile data
+        // console.log('Profile Data:', profileData);
         
-        await browser.close();
+        // await browser.close();
 
     }catch(error) {
         console.log(error)
@@ -36,3 +47,29 @@ export async function POST(request) {
 
     return NextResponse.json("");
 }
+
+
+// Fetch LinkedIn profile data
+// const profileResponse = await fetch('https://api.linkedin.com/v2/me', {
+//     headers: {
+//       Authorization: `Bearer ${access_token}`,
+//     },
+//   });
+//   const profileData = await profileResponse.json();
+//   console.log('profileData', profileData);
+
+//   // Fetch LinkedIn email data
+//   const emailResponse = await fetch('https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))', {
+//     headers: {
+//       Authorization: `Bearer ${access_token}`,
+//     },
+//   });
+//   const emailData = await emailResponse.json();
+//   console.log('emailData', emailData)
+// const emailAddress = emailData.elements[0]['handle~'].emailAddress;
+
+//   // Return the profile and email data
+//   return NextResponse.json({
+//     profile: profileData,
+//     email: emailAddress,
+//   });
